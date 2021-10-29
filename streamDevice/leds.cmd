d38ea4786b5b1f65778f45fc206b9b0e5b171acc
Scott Feister <sfeister@gmail.com>
2021-10-28 17:04:00 -0700

    Removed *.cmd from .gitignore since it was in the way of command file extensions in EPICS for StreamDevice

---
+++ streamDevice/leds.cmd
@@ -0,0 +1,26 @@
+exec O.$EPICS_HOST_ARCH/streamApp $0
+dbLoadDatabase "O.Common/streamApp.dbd"
+streamApp_registerRecordDeviceDriver
+
+
+epicsEnvSet "STREAM_PROTOCOL_PATH","."
+
+drvAsynSerialPortConfigure("led_ino","/dev/ttyACM0")
+asynSetOption("led_ino",0,"baud","9600")
+asynSetOption("led_ino",0,"bits","8")
+asynSetOption("led_ino",0,"parity","none")
+asynSetOption("led_ino",0,"stop","1")
+asynSetOption("led_ino",0,"clocal","Y")
+asynSetOption("led_ino",0,"crtscts","N")
+
+#drvAsynIPPortConfigure "LO","localhost:40000"
+#vxi11Configure "LO","192.168.1.236",0,0.0,"gpin0"
+
+dbLoadRecords "leds.db"
+
+#log debug output to file
+#streamSetLogfile StreamDebug.log
+
+iocInit
+
+#var streamDebug 1
