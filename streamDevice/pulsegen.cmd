486679abc3897150ef85d14401c5aad505a1dffd
Scott Feister <sfeister@gmail.com>
2021-11-04 10:49:53 -0700

    Created StreamDevice files for all output devices in the sidekick system; somewhat tested, more testing needed

---
+++ streamDevice/pulsegen.cmd
@@ -0,0 +1,26 @@
+exec O.$EPICS_HOST_ARCH/streamApp $0
+dbLoadDatabase "O.Common/streamApp.dbd"
+streamApp_registerRecordDeviceDriver
+
+
+epicsEnvSet "STREAM_PROTOCOL_PATH","."
+
+drvAsynSerialPortConfigure("pulsegen_ino","/dev/ttyUSB0")
+asynSetOption("pulsegen_ino",0,"baud","9600")
+asynSetOption("pulsegen_ino",0,"bits","8")
+asynSetOption("pulsegen_ino",0,"parity","none")
+asynSetOption("pulsegen_ino",0,"stop","1")
+asynSetOption("pulsegen_ino",0,"clocal","Y")
+asynSetOption("pulsegen_ino",0,"crtscts","N")
+
+#drvAsynIPPortConfigure "LO","localhost:40000"
+#vxi11Configure "LO","192.168.1.236",0,0.0,"gpin0"
+
+dbLoadRecords "pulsegen.db"
+
+#log debug output to file
+#streamSetLogfile StreamDebug.log
+
+iocInit
+
+#var streamDebug 1
