bc8297e58e6de70820a039a9cc08d953e980c5c8
Scott Feister <sfeister@gmail.com>
2022-04-05 15:56:30 -0700

    Added five second delay to startup scripts to workaround initial Arduino commands timing out on first plugins

--- streamDevice/leds.cmd
+++ streamDevice/leds.cmd
@@ -16,6 +16,8 @@ asynSetOption("led_ino",0,"crtscts","N")
 #drvAsynIPPortConfigure "LO","localhost:40000"
 #vxi11Configure "LO","192.168.1.236",0,0.0,"gpin0"
 
+epicsThreadSleep(5) # empirically necessary to pause here, to avoid communication timeout on first SCPI commands to Arduinos. -SKF April 5 2022
+
 dbLoadRecords "leds.db"
 
 #log debug output to file
