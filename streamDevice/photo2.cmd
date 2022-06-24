599e82aa5a040c9c7dd0453afd4dbd008b13b0fa
Scott Feister <sfeister@gmail.com>
2022-06-24 14:10:59 -0700

    Added a second photodetector device to the sidekick system so we can examine shot-to-shot correlations between two detectors

---
+++ streamDevice/photo2.cmd
@@ -0,0 +1,28 @@
+exec O.$EPICS_HOST_ARCH/streamApp $0
+dbLoadDatabase "O.Common/streamApp.dbd"
+streamApp_registerRecordDeviceDriver
+
+
+epicsEnvSet "STREAM_PROTOCOL_PATH","."
+
+drvAsynSerialPortConfigure("photo2_ino","/dev/ttyUSB1")
+asynSetOption("photo2_ino",0,"baud","9600")
+asynSetOption("photo2_ino",0,"bits","8")
+asynSetOption("photo2_ino",0,"parity","none")
+asynSetOption("photo2_ino",0,"stop","1")
+asynSetOption("photo2_ino",0,"clocal","Y")
+asynSetOption("photo2_ino",0,"crtscts","N")
+
+#drvAsynIPPortConfigure "LO","localhost:40000"
+#vxi11Configure "LO","192.168.1.236",0,0.0,"gpin0"
+
+epicsThreadSleep(5) # empirically necessary to pause here, to avoid communication timeout on first SCPI commands to Arduinos. -SKF April 5 2022
+
+dbLoadRecords "photo2.db"
+
+#log debug output to file
+#streamSetLogfile StreamDebug.log
+
+iocInit
+
+#var streamDebug 1
